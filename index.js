30bb2e656596ba1c47c183ea4993e6854219a9e8
RafiKastner <rafikastner@gmail.com>
2024-03-11 17:19:13 -0400

    Broke stuff ig

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ app.listen(3000, () => console.log('listening at 3000'));
 app.use(express.static('public'));
 
 app.post('/api', (request, response) => {
-    console.log(request);
+    console.log(request.body);
 });
